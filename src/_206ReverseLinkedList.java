d7c33af0d8a31ad709ed5ae3b4c3d01f6bb31af6
Li Chen <chen1649chenli@gmail.com>
2019-07-10 23:00:53 -0400

    Reverse Linked List using recursion

--- src/_206ReverseLinkedList.java
+++ src/_206ReverseLinkedList.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 public class _206ReverseLinkedList {
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int v){
+            this.val = v;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode nextNode = head.next;
+        ListNode newHead = reverseList(nextNode);
+        nextNode.next = head;
+        head.next = null;
+        return newHead;
+    }
 }
