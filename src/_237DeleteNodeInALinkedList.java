3256533fe92ad5b26cd839eb79fab3dfa80fe105
Li Chen <chen1649chenli@gmail.com>
2019-07-11 11:31:40 -0400

    Delete Node in A Linked List

--- src/_237DeleteNodeInALinkedList.java
+++ src/_237DeleteNodeInALinkedList.java
@@ -1,11 +1,7 @@
 public class _237DeleteNodeInALinkedList {
     public void deleteNode(ListNode node) {
-        ListNode ptr = node;
-        while(ptr.next.next != null){
-            ptr.val = ptr.next.val;
-            ptr = ptr.next;
-        }
-        ptr.val = ptr.next.val;
-        ptr.next = null;
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
+
 }
