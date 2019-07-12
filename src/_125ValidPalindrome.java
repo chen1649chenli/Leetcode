431e4b8e4c126fc9430e2676ee3b624b31c710df
Li Chen <chen1649chenli@gmail.com>
2019-07-12 11:11:15 -0400

    Valid Palindrome using two pointer

--- src/_125ValidPalindrome.java
+++ src/_125ValidPalindrome.java
@@ -1,7 +1,25 @@
 public class _125ValidPalindrome {
     public boolean isPalindrome(String s) {
-        String s_ = s.replaceAll("[^a-zA-Z0-9]]","").toLowerCase();
-        String t = new StringBuilder(s_).reverse().toString();
-        return s_.equals(t);
+        if (s.isEmpty()) return true;
+        int head = 0, tail = s.length() -1;
+        char headChar, tailChar;
+        while(head < tail){
+            headChar = s.charAt(head);
+            tailChar = s.charAt(tail);
+            if (!Character.isLetterOrDigit(headChar)){
+                head += 1;
+            }else if(!Character.isLetterOrDigit(tailChar)){
+                tail -= 1;
+            } else{
+                if (Character.toLowerCase(headChar) == Character.toLowerCase(tailChar)){
+                    tail -= 1;
+                    head += 1;
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
