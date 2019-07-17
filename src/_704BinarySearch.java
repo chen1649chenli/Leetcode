1101c3f5deb0e34e63917e70969ff579cd071d12
Li Chen <chen1649chenli@gmail.com>
2019-07-17 14:48:38 -0400

    Binary Search

---
+++ src/_704BinarySearch.java
@@ -0,0 +1,17 @@
+public class _704BinarySearch {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target){
+                return mid;
+            }else if (nums[mid] > target){
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
