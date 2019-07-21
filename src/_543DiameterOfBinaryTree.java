f40ebfd13da76465b7ed0efd3072ef3ee40677a1
Li Chen <chen1649chenli@gmail.com>
2019-07-21 11:27:14 -0400

    543 Diameter of Binary Tree

--- src/_543DiameterOfBinaryTree.java
+++ src/_543DiameterOfBinaryTree.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class _543DiameterOfBinaryTree {
+    int max;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        calculateMax(root);
+        return this.max;
+
+    }
+
+    private int calculateMax(TreeNode node){
+        if (node == null) return 0;
+        int leftDepth = calculateMax(node.left);
+        int rightDepth = calculateMax(node.right);
+        this.max = Math.max(this.max, leftDepth + rightDepth);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
 }
