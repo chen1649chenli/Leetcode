73d4cd050e08a056e4c6e2c73d00eaddff52cf45
Li Chen <chen1649chenli@gmail.com>
2019-07-21 16:19:36 -0400

    101 SymmetricTree using recursive method

--- src/_101SymmetricTree.java
+++ src/_101SymmetricTree.java
@@ -6,21 +6,13 @@ import java.util.Queue;
 public class _101SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root.left);
-        queue.offer(root.right);
-        while(queue.size() > 1){
-            TreeNode left = queue.poll();
-            TreeNode right = queue.poll();
-            if (left == null && right == null) continue;
-            if (left == null ^ right == null) return false;
-            if (left.val != right.val ) return false;
-            queue.offer(left.left);
-            queue.offer(right.right);
-            queue.offer(left.right);
-            queue.offer(right.left);
-        }
-        return true;
+        return helper(root.left, root.right);
     }
 
+    private boolean helper(TreeNode left, TreeNode right){
+        if (left == null && right == null) return true;
+        if (left == null ^ right == null) return false;
+        if (left.val != right.val) return false;
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
 }
