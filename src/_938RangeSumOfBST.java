a6d0e59515aa4e332f385c0102210c2f0dac8703
Li Chen <chen1649chenli@gmail.com>
2019-07-22 09:14:29 -0400

    938 Range Sum of BST - better recursive

--- src/_938RangeSumOfBST.java
+++ src/_938RangeSumOfBST.java
@@ -1,18 +1,11 @@
 public class _938RangeSumOfBST {
     public int rangeSumBST(TreeNode root, int L, int R) {
-        int sum = 0;
-        if (root != null) sum = helper(root, sum, L, R);
-        return sum;
-    }
-
-    private int helper(TreeNode node, int val, int L, int R){
-        if (node == null) return val;
-        if (node.val >=L && node.val <= R) val += node.val;
-
-
-        val = helper(node.left, val, L, R);
-        val = helper(node.right, val, L, R);
-
-        return val;
+        /** Add base case */
+        if (root == null) return 0;
+        /** Exclude left branch if node val < L*/
+        if (root.val < L) return rangeSumBST(root.right, L, R);
+        /** Exclude right branch if node val > R*/
+        if (root.val > R) return rangeSumBST(root.left, L, R);
+        return root.val + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R);
     }
 }
