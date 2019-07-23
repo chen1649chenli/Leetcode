41e7d653d09e816366d3de3a9a44ba856da8b965
Li Chen <chen1649chenli@gmail.com>
2019-07-23 11:47:23 -0400

    169 Majority Element using bit manipulation

--- src/_169MajorityElement.java
+++ src/_169MajorityElement.java
@@ -3,14 +3,26 @@ import java.util.Map;
 
 public class _169MajorityElement {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[32];
+        for (int num: nums){
+            for (int j = 0; j < 32; j += 1){
+                int res = getBit(num, j);
+                if (res == 0) ans[j]++;
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < 32; i += 1){
+            res = (ans[i] > nums.length /2) ? res: setBit(res, i);
+        }
+        return res;
+    }
 
-        for (int i = 0; i < nums.length; i += 1){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        for (int i: map.keySet()){
-            if (map.get(i) > nums.length / 2) return i;
-        }
-        return 0;
+    private int getBit(int num, int pos){
+        return ((num & (1 << pos)) == 0) ? 0: 1;
+    }
+
+    private int setBit(int num, int pos){
+        int mask = 1<<pos;
+        return num|mask;
     }
 }
