bca58fd84aa4f72cee79e6743337e8ffcc58938e
Li Chen <chen1649chenli@gmail.com>
2019-07-27 09:57:24 -0400

    455 Assign Cookies using two pointer

--- src/_455AssignCookies.java
+++ src/_455AssignCookies.java
@@ -1,28 +1,17 @@
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class _455AssignCookies {
     public int findContentChildren(int[] g, int[] s) {
-        PriorityQueue<Integer> greedy = new PriorityQueue<>();
-        PriorityQueue<Integer> size = new PriorityQueue<>();
-        for (int i: g){
-            greedy.add(i);
-        }
-        for (int j: s){
-            size.add(j);
-        }
-        int sum = 0;
-
-
-        while(greedy.size() > 0 && size.size() > 0){
-            if (greedy.peek() > size.peek()){
-                size.poll();
-            }else{
-                greedy.poll();
-                size.poll();
-                sum += 1;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int cookie, child = 0;
+        for (cookie = 0; cookie < s.length; cookie++){
+            if (child < g.length && s[cookie] >= g[child]){
+                child++;
             }
         }
-        return sum;
+        return child;
     }
 }
