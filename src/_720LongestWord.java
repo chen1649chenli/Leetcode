05760c9c356dc54d4cb95c099f5c419c08a21572
Li Chen <chen1649chenli@gmail.com>
2019-07-27 19:39:46 -0400

    _720 Longest Word - simple HashSet solution

--- src/_720LongestWord.java
+++ src/_720LongestWord.java
@@ -4,19 +4,17 @@ import java.util.Set;
 
 public class _720LongestWord {
     public String longestWord(String[] words) {
-        public String longestWord(String[] words) {
-            Set<String> set = new HashSet<>();
-            Arrays.sort(words);
-            String ans = "";
-            for (String str: words){
-                if (str.length() == 1 || set.contains(str.substring(0, str.length() - 1))){
-                    set.add(str);
-                    if (str.length() > ans.length()){
-                        ans = str;
-                    }
+        Set<String> set = new HashSet<>();
+        Arrays.sort(words);
+        String ans = "";
+        for (String str: words){
+            if (str.length() == 1 || set.contains(str.substring(0, str.length() - 1))){
+                set.add(str);
+                if (str.length() > ans.length()){
+                    ans = str;
                 }
             }
-            return ans;
         }
+        return ans;
     }
 }
