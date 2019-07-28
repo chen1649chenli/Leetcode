515a242c59118d170613d7aa51b93a3fc947817e
Li Chen <chen1649chenli@gmail.com>
2019-07-28 11:05:03 -0400

    1065 Index Pairs of a String uisng brute force

--- src/_1065IndexPairsOfAString.java
+++ src/_1065IndexPairsOfAString.java
@@ -1,24 +1,7 @@
 import java.util.*;
 
 public class _1065IndexPairsOfAString {
-    private static class TreeNode {
-        boolean end;
-        TreeNode[] children = new TreeNode[26];
-
-        public void insert(String str){
-            TreeNode currNode = this;
-            for (char c: str.toCharArray()){
-                if (currNode.children[c - 'a'] == null){
-                    currNode.children[c - 'a'] = new TreeNode();
-                }
-                currNode = currNode.children[c - 'a'];
-            }
-            currNode.end = true;
-        }
-    }
-
     public int[][] indexPairs(String text, String[] words) {
-        TreeNode root = new TreeNode();
         PriorityQueue<int[]> ans = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -29,27 +12,19 @@ public class _1065IndexPairsOfAString {
                 }
             }
         });
-        for (String word: words){
-            root.insert(word);
-        }
-        for (int i = 0; i < text.length(); i += 1){
-            TreeNode curr = root;
-            int j = i;
-            while(j < text.length()  && curr.children[text.charAt(j) - 'a'] != null){
-                curr = curr.children[text.charAt(j) - 'a'];
-                if (curr.end){
-                    ans.add(new int[]{i, j});
-                    System.out.println(i + " " + j);
+
+        for (int i = 0; i < text.length(); i++){
+            for (String word: words){
+                if (text.startsWith(word, i)){
+                    ans.add(new int[]{i, i + word.length() - 1});
                 }
-                j += 1;
             }
         }
-
         int size = ans.size();
-        int[][] res = new int[size][2];
+        int[][] result = new int[size][2];
         for (int i = 0; i < size; i ++){
-            res[i] = ans.poll();
+            result[i] = ans.poll();
         }
-        return res;
+        return result;
     }
 }
