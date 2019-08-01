50f593d25e6f440ace3b9e39177b013b9d74e869
Li Chen <chen1649chenli@gmail.com>
2019-08-01 19:34:18 -0400

    3 longest Substring without repeating characters using sliding window

--- src/_003LongestSubstringWithoutRepeatingCharacters.java
+++ src/_003LongestSubstringWithoutRepeatingCharacters.java
@@ -2,22 +2,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class _003LongestSubstringWithoutRepeatingCharacters {
-
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) return 0;
-        int ans = 1;
-        for (int i = 0; i < s.length(); i += 1){
-            Set<Character> uniqChar = new HashSet<>();
-            uniqChar.add(s.charAt(i));
-            for (int j = i + 1; j < s.length(); j += 1){
-                if (!uniqChar.contains(s.charAt(j))){
-                    uniqChar.add(s.charAt(j));
-                    if (j - i + 1 > ans) ans = j - i + 1;
-                }else{
-                    break;
-                }
+        int slow = 0, quick = 0, max = 0;
+        Set<Character> set = new HashSet<>();
+        while(quick < s.length()){
+            if (!set.contains(s.charAt(quick))){
+                set.add(s.charAt(quick));
+                quick += 1;
+                max = Math.max(max, quick - slow);
+            }else{
+                set.remove(s.charAt(slow++));
             }
         }
-        return ans;
+        return max;
     }
 }
