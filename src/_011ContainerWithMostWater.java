1441822592df60386b28b85702b03ef7b7e669a7
Li Chen <chen1649chenli@gmail.com>
2019-08-03 14:45:40 -0400

    11 Container with Most Water

--- src/_011ContainerWithMostWater.java
+++ src/_011ContainerWithMostWater.java
@@ -4,10 +4,13 @@ import java.util.PriorityQueue;
 public class _011ContainerWithMostWater {
     public int maxArea(int[] height) {
         int res = Integer.MIN_VALUE;
-        for (int i = 0; i < height.length - 1; i += 1){
-            for (int j = i + 1; j < height.length; j += 1){
-                int tmp = (j - i) * Math.min(height[i], height[j]);
-                res = Math.max(tmp, res);
+        int i = 0, j = height.length - 1;
+        while(i < j){
+            res = Math.max(res, (j - i) * Math.min(height[i], height[j]));
+            if (height[i] < height[j]){
+                i += 1;
+            }else {
+                j -= 1;
             }
         }
         return res;
