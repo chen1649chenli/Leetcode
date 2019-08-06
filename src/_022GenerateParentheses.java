f6812e2420d38b71d57f0bc323e0901bb3f427dc
Li Chen <chen1649chenli@gmail.com>
2019-08-06 17:50:04 -0400

    22 Generate Parentheses using recursive method

--- src/_022GenerateParentheses.java
+++ src/_022GenerateParentheses.java
@@ -3,56 +3,22 @@ import java.util.*;
 public class _022GenerateParentheses {
     public List<String> generateParenthesis(int n) {
         if (n == 0) return new ArrayList<String>();
-
-        char[] chs = new char[2 * n];
-        for (int i = 0; i < 2* n; i += 1){
-            chs[i] = ' ';
-        }
-        chs[0] = '(';
-        Queue<String> q = new LinkedList<>();
-        q.offer(String.valueOf(chs));
-        for (int i = 1; i < 2*n; i++){
-            int size = q.size();
-            for (int j = 0; j < size; j++){
-                String s = q.poll();
-                if (validLeft(s, i)){
-                    char[] chs_ = s.toCharArray();
-                    chs_[i] = '(';
-                    q.offer(String.valueOf(chs_));
-                }
-                if (validRight(s, i)){
-                    char[] chs_ = s.toCharArray();
-                    chs_[i] = ')';
-                    q.offer(String.valueOf(chs_));
-                }
-            }
-        }
-        return new LinkedList<>(q);
+        List<String> str = new ArrayList<>();
+        helper(str, "", 0, 0, n);
+        return str;
     }
 
-    private boolean validLeft(String s, int i){
-        int count = 0;
-        for (int k = 0; k < i; k++){
-            if(s.charAt(k) == '('){
-                count ++;
-            }
+    private void helper(List<String> str_, String s, int i, int j, int n){
+        if (i == n && j == n){
+            str_.add(s);
+            return;
         }
-        return (count < s.length() / 2);
-    }
+        if (i < n){
+            helper(str_, s + "(", i+1, j, n);
+        }
 
-    private boolean validRight(String s, int j){
-        Stack<Character> stack = new Stack<>();
-        for (int k = 0; k < j; k++){
-            if (s.charAt(k) == '('){
-                stack.push('(');
-            }else{
-                stack.pop();
-            }
-        }
-        if (!stack.isEmpty() && stack.peek() =='('){
-            return true;
-        }else{
-            return false;
+        if ( j < i){
+            helper(str_, s+ ")", i, j+1, n);
         }
     }
 }
