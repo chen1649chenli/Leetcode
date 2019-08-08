ca521ac496bed4e06bfb407d3333e7c691b38b9d
Li Chen <chen1649chenli@gmail.com>
2019-08-08 11:14:37 -0400

    560 Subarray Sum Equals K using Hashmap and Presum

--- src/_560SubarraySumEqualsK.java
+++ src/_560SubarraySumEqualsK.java
@@ -1,16 +1,18 @@
+import java.util.*;
+
 public class _560SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
-        int size = nums.length;
-        int count = 0;
-        for (int i = 0; i < size; i++){
-            int sum = 0;
-            for (int j = i; j < size; j ++){
-                sum += nums[j];
-                if (sum == k){
-                    count += 1;
-                }
+        int size = nums.length, sum = 0, count = 0;
+        Map<Integer, Integer> dict = new HashMap<>();
+
+        dict.put(0, 1);
+        for (int i = 0; i < size; i++) {
+            sum += nums[i];
+            if (dict.containsKey(sum - k)){
+                count = count + dict.get(sum - k);
             }
+            dict.put(sum, dict.getOrDefault(sum, 0) + 1);
         }
-        return count;
+         return count;
     }
 }
