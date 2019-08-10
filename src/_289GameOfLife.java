ac22a3e211aa2f42bc8a436870ce6c56e558991f
Li Chen <chen1649chenli@gmail.com>
2019-08-10 13:46:16 -0400

    289 Game of Life solving in place

--- src/_289GameOfLife.java
+++ src/_289GameOfLife.java
@@ -2,42 +2,50 @@ public class _289GameOfLife {
     public void gameOfLife(int[][] board) {
         if (board == null || board.length == 0) return;
 
+        /** row number*/
+        int m = board.length;
 
-        int row = board.length;
-        int col = board[0].length;
-        int[][] original = new int[row][col];
+        /** column number*/
+        int n = board[0].length;
 
-        for (int i = 0; i < row; i++){
-            for (int j = 0; j < col; j++){
-                original[i][j] = board[i][j];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                int count = liveNeighbor(board, i, j, m, n);
+                if (board[i][j] == 1 && count < 2){
+                    board[i][j] = 1;
+                } else if(board[i][j] == 1 && (count == 2 || count == 3)){
+                    board[i][j] = 3;
+                } else if(board[i][j] == 1 && count > 3 ){
+                    board[i][j] = 1;
+                } else if (board[i][j] == 0 && count == 3){
+                    board[i][j] = 2;
+                }
             }
         }
-        for (int i = 0; i < row; i++){
-            for (int j = 0; j < col; j++){
-                update(board, original, i, j);
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (board[i][j] == 2){
+                    board[i][j] = 1;
+                }else if (board[i][j] == 1){
+                    board[i][j] = 0;
+                }else if (board[i][j] == 3){
+                    board[i][j] = 1;
+                }
             }
         }
     }
 
-    private void update(int[][] board, int[][] original, int i, int j){
-        int row = board.length - 1;
-        int col = board[0].length - 1;
+    private int liveNeighbor(int[][] board, int i, int j, int m, int n){
+        int[][] neighbors = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
         int count = 0;
-        if (i > 0 && original[i - 1][j] == 1) count++;
-        if (i < row && original[i + 1][j] == 1) count ++;
-        if (j > 0 && original[i][j - 1] == 1) count++;
-        if (j < col && original[i][j + 1] == 1) count++;
-        if (i > 0 && j > 0 && original[i - 1][j - 1] == 1) count ++;
-        if (i < row && j > 0 && original[i+1][j - 1] == 1) count ++;
-        if (i > 0 && j < col && original[i -1][j+1] == 1) count ++;
-        if (i < row && j < col && original[i + 1][j + 1] == 1) count++;
+        for (int[] neighbor: neighbors){
+            int row = neighbor[0] + i;
+            int col = neighbor[1] + j;
 
-        if (count < 2){
-            board[i][j] = 0;
-        }else if(count == 3){
-            board[i][j] = 1;
-        }else if(count > 3){
-            board[i][j] = 0;
+            if (row >=0 && row < m && col >= 0 && col < n){
+                if (board[row][col] == 1 ||board[row][col] == 3) count ++;
+            }
         }
+        return count;
     }
 }
