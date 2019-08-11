7e44fad4daf6e5189fc0027534bd0eb8e36d7bb9
Li Chen <chen1649chenli@gmail.com>
2019-08-10 20:05:42 -0400

    31 Next Permutation refactoring

--- src/_031NextPermutation.java
+++ src/_031NextPermutation.java
@@ -3,37 +3,35 @@ import java.util.Arrays;
 public class _031NextPermutation {
     public void nextPermutation(int[] nums) {
         if (nums == null || nums.length < 2) return;
-        int pos = findIdx(nums);
-        if (pos != 0){
-            change(nums, pos);
-        }
-        arrange(nums, pos, nums.length - 1);
-    }
-
-    private int findIdx(int[] nums){
-        int h = nums.length - 1;
-        while(h >= 1 && nums[h] <= nums[h-1]){
-            h--;
-        }
-        return h;
-    }
-
-    private void arrange(int[] nums, int l, int h){
-        Arrays.sort(nums, l, h+1);
-    }
-
-    private void change(int[] nums, int pos){
-        int l = nums[pos - 1];
-        int idx = pos;
-        int num = nums[pos];
-        for (int i = pos; i < nums.length; i++){
-            if (nums[i] < num && nums[i] > l){
-                idx = i;
-                num = nums[i];
+        int firstSmall = -1;
+        for (int i = nums.length - 2;  i >= 0; i--){
+            if (nums[i] < nums[i+1]){
+                firstSmall = i;
+                break;
             }
         }
-        int swap = nums[idx];
-        nums[idx] = l;
-        nums[pos-1] = swap;
+        if (firstSmall == -1){
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+        for (int i = nums.length - 1; i > firstSmall; i--){
+            if (nums[i] > nums[firstSmall]){
+                swap(nums, i, firstSmall);
+                break;
+            }
+        }
+        reverse(nums, firstSmall+ 1, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int swap = nums[i];
+        nums[i] = nums[j];
+        nums[j] = swap;
+    }
+
+    private void reverse(int[] nums, int i, int j){
+        while(i < j){
+            swap(nums, i++, j--);
+        }
     }
 }
