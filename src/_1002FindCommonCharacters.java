775d553edd846bc9f04066d87bba73d189110d61
Li Chen <chen1649chenli@gmail.com>
2019-08-13 16:13:37 -0400

    1002 Find Common Characters using int array

--- src/_1002FindCommonCharacters.java
+++ src/_1002FindCommonCharacters.java
@@ -3,28 +3,26 @@ import java.util.List;
 
 public class _1002FindCommonCharacters {
     public List<String> commonChars(String[] A) {
-        List<char[]> data = new ArrayList<>();
         List<String> ans = new ArrayList<>();
         if (A == null || A.length == 0) return ans;
-        int size = A.length;
+        int[] count = new int[26];
 
-        for (String str: A){
-            data.add(str.toCharArray());
+        for (int i = 0; i < A[0].length(); i++){
+            count[A[0].charAt(i) - 'a']++;
         }
-        char[] chars = A[0].toCharArray();
-        for(char c: chars){
-            int count = 0;
-            for (char[] arr_: data){
-                for (int i = 0; i < arr_.length;i++){
-                    if (arr_[i] == c){
-                        count++;
-                        arr_[i] = '$';
-                        break;
-                    }
-                }
+
+        for (int i = 1; i < A.length; i++){
+            int[] tmpCount = new int[26];
+            for (int j = 0; j < A[i].length(); j++){
+                tmpCount[A[i].charAt(j) - 'a']++;
             }
-            if (count == size){
-                ans.add(String.valueOf(c));
+            for (int j = 0; j < 26; j++){
+                count[j] = Math.min(count[j], tmpCount[j]);
+            }
+        }
+        for (int i = 0; i < 26; i++){
+            for (int j = 0; j < count[i]; j++){
+                ans.add(Character.toString((char) (i + 'a')));
             }
         }
         return ans;
