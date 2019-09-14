e39039d6ad8301b925ead17114ba7b183ff1f894
Li Chen <chen1649chenli@gmail.com>
2019-09-14 15:51:41 -0400

    144 Binary Preorder Traversal using iterative solution

--- src/_144BinaryTreePreorderTraversal.java
+++ src/_144BinaryTreePreorderTraversal.java
@@ -1,17 +1,21 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class _144BinaryTreePreorderTraversal {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        preorderTraversal(root, ans);
+        Deque<TreeNode> q = new ArrayDeque<>();
+        if (root == null) return ans;
+        while(root != null || !q.isEmpty()){
+            while(root != null){
+                ans.add(root.val);
+                q.push(root);
+                root = root.left;
+            }
+            root = q.pop().right;
+        }
         return ans;
-    }
-
-    private void preorderTraversal(TreeNode node, List<Integer> ans){
-        if (node == null) return;
-        ans.add(node.val);
-        preorderTraversal(node.left, ans);
-        preorderTraversal(node.right, ans);
     }
 }
