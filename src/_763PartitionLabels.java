d2463a80b3e2d93613b30ab9c26a1e87ea18009c
Li Chen <chen1649chenli@gmail.com>
2019-09-19 15:53:15 -0400

    763 Partition labels using two pointers

--- src/_763PartitionLabels.java
+++ src/_763PartitionLabels.java
@@ -4,39 +4,21 @@ public class _763PartitionLabels {
     public List<Integer> partitionLabels(String S) {
         List<Integer>ansList = new ArrayList<>();
         if (S == null || S.length() == 0) return ansList;
-
-        Map<Character, int[]> map = new HashMap<>();
+        int[] last = new int[26];
         for (int i = 0; i < S.length(); i += 1){
-            if (!map.containsKey(S.charAt(i))){
-                map.put(S.charAt(i), new int[]{i, i});
-            }else{
-                int[] tmp = map.get(S.charAt(i));
-                tmp[1] = i;
-                map.put(S.charAt(i), tmp);
+            last[S.charAt(i) - 'a'] = i;
+        }
+        int start = -1, end = -1;
+        for (int i = 0; i < S.length(); i += 1){
+            if (start == -1){
+                start = i;
+            }
+            end = Math.max(end, last[S.charAt(i) - 'a']);
+            if (end == i){
+                ansList.add(end - start + 1);
+                start = -1;
             }
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-        for (char c: map.keySet()){
-            pq.add(map.get(c));
-        }
-
-        int[] prev = pq.poll();
-
-        while(pq.size() > 0){
-            int[] curr = pq.poll();
-            if (curr[0] < prev[1]){
-                prev[1] = Math.max(prev[1], curr[1]);
-            }else{
-                ansList.add(prev[1] - prev[0] + 1);
-                prev = curr;
-            }
-        }
-        ansList.add(prev[1] - prev[0] + 1);
         return ansList;
     }
 }
