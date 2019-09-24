5dbc65117652db5f336d6562c3ef8f430e3faff7
Li Chen <chen1649chenli@gmail.com>
2019-09-24 11:56:11 -0400

    362 Design Hit Counter using bucket

--- src/_362DesignHitCounter.java
+++ src/_362DesignHitCounter.java
@@ -2,26 +2,37 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class _362DesignHitCounter {
-    Queue<Integer> q;
+    int[] hitCount;
+    int[] times;
 
     /** Initialize your data structure here. */
-    public _362DesignHitCounter() {
-        q = new LinkedList<>();
+    public HitCounter() {
+        hitCount = new int[300];
+        times = new int[300];
     }
 
     /** Record a hit.
      @param timestamp - The current timestamp (in seconds granularity). */
     public void hit(int timestamp) {
-        q.offer(timestamp);
+        int idx = timestamp % 300;
+        if (times[idx] != timestamp){
+            hitCount[idx] = 1;
+            times[idx] = timestamp;
+        }else{
+            hitCount[idx] += 1;
+        }
     }
 
     /** Return the number of hits in the past 5 minutes.
      @param timestamp - The current timestamp (in seconds granularity). */
     public int getHits(int timestamp) {
-
-        while(!q.isEmpty() && q.peek() <= timestamp - 300){
-            q.poll();
+        int total = 0;
+        int cutoff = timestamp - 300;
+        for (int i = 0; i < 300; i += 1){
+            if (times[i] > cutoff){
+                total += hitCount[i];
+            }
         }
-        return q.size();
+        return total;
     }
 }
