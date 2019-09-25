4787c7ebc7f6f6581e99670cef1027d4abd8245b
Li Chen <chen1649chenli@gmail.com>
2019-09-25 12:43:04 -0400

    127 Word Ladder using two BFS

--- src/_127WordLadder.java
+++ src/_127WordLadder.java
@@ -3,35 +3,44 @@ import java.util.*;
 public class _127WordLadder {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> dict = new HashSet<>(wordList);
-        Queue<String> q = new LinkedList<>();
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
         Set<String> visited = new HashSet<>();
-        q.offer(beginWord);
-        q.offer(null);
-        visited.add(beginWord);
-        int step = 1;
-        if (!dict.contains(endWord)) return 0;
 
-        while(q.size() > 1){
-            String str = q.poll();
-            if (str == null){
-                q.offer(null);
-                step += 1;
-                continue;
+        if (!dict.contains(endWord)) return 0;
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        visited.add(beginWord);
+        visited.add(endWord);
+
+        int step = 1;
+        while(!beginSet.isEmpty() && !endSet.isEmpty()){
+            if(beginSet.size() > endSet.size()){
+                Set<String> tmp = beginSet;
+                beginSet = endSet;
+                endSet = tmp;
             }
-            for (int i = 0; i < str.length(); i += 1){
-                char[] chars = str.toCharArray();
-                for (char c = 'a'; c <= 'z'; c += 1){
-                    chars[i] = c;
-                    String word = new String(chars);
-                    if (word.equals(endWord)){
-                        return step + 1;
-                    }
-                    if (dict.contains(word) && !visited.contains(word)){
-                        q.offer(word);
-                        visited.add(word);
+
+            Set<String> tmp = new HashSet<>();
+            for (String word: beginSet){
+                for (int i = 0; i < word.length(); i += 1){
+                    char[] chars = word.toCharArray();
+                    for (char c = 'a'; c <= 'z'; c += 1){
+                        chars[i] = c;
+                        String str = new String(chars);
+
+                        if(endSet.contains(str)){
+                            return step + 1;
+                        }
+                        if (!visited.contains(str) && dict.contains(str)){
+                            tmp.add(str);
+                            visited.add(str);
+                        }
                     }
                 }
             }
+            step += 1;
+            beginSet = tmp;
         }
         return 0;
     }
