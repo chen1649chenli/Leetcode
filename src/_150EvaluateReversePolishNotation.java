9a3409450def88588ed15fafaa6aa66e63f4aded
Li Chen <chen1649chenli@gmail.com>
2019-10-03 11:41:27 -0500

    150 Evaluate Reverse Polish Notation

--- src/_150EvaluateReversePolishNotation.java
+++ src/_150EvaluateReversePolishNotation.java
@@ -1,24 +1,26 @@
+import java.util.*;
 public class _150EvaluateReversePolishNotation {
-    Deque<Integer> stack = new ArrayDeque<>();
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
         for (String str : tokens){
-        if (str.equals("+")){
-            int res = stack.pop() + stack.pop();
-            stack.push(res);
-        }else if (str.equals("-")){
-            int res = -stack.pop() + stack.pop();
-            stack.push(res);
-        }else if (str.equals("*")){
-            int res = stack.pop() * stack.pop();
-            stack.push(res);
-        }else if (str.equals("/")){
-            int _1 = stack.pop();
-            int _2 = stack.pop();
-            int res = _2 / _1;
-            stack.push(res);
-        }else {
-            stack.push(Integer.parseInt(str));
+            if (str.equals("+")){
+                int res = stack.pop() + stack.pop();
+                stack.push(res);
+            }else if (str.equals("-")){
+                int res = -stack.pop() + stack.pop();
+                stack.push(res);
+            }else if (str.equals("*")){
+                int res = stack.pop() * stack.pop();
+                stack.push(res);
+            }else if (str.equals("/")){
+                int _1 = stack.pop();
+                int _2 = stack.pop();
+                int res = _2 / _1;
+                stack.push(res);
+            }else {
+                stack.push(Integer.parseInt(str));
+            }
         }
-    }
         return stack.pop();
-}
+    }
 }
