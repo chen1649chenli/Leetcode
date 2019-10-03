0a0032b3869d21f0f7e19df73c1df903c7bfb45b
Li Chen <chen1649chenli@gmail.com>
2019-10-03 11:25:38 -0500

    150 Evaluate Reverse Polish Notation

---
+++ src/_150EvaluateReversePolishNotation.java
@@ -0,0 +1,24 @@
+public class _150EvaluateReversePolishNotation {
+    Deque<Integer> stack = new ArrayDeque<>();
+        for (String str : tokens){
+        if (str.equals("+")){
+            int res = stack.pop() + stack.pop();
+            stack.push(res);
+        }else if (str.equals("-")){
+            int res = -stack.pop() + stack.pop();
+            stack.push(res);
+        }else if (str.equals("*")){
+            int res = stack.pop() * stack.pop();
+            stack.push(res);
+        }else if (str.equals("/")){
+            int _1 = stack.pop();
+            int _2 = stack.pop();
+            int res = _2 / _1;
+            stack.push(res);
+        }else {
+            stack.push(Integer.parseInt(str));
+        }
+    }
+        return stack.pop();
+}
+}
