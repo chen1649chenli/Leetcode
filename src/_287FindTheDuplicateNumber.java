c77d43a951840c83e7ce7cf8dafddfa1c3866278
Li Chen <chen1649chenli@gmail.com>
2019-11-19 20:17:32 -0500

    287 cleaner solution

--- src/_287FindTheDuplicateNumber.java
+++ src/_287FindTheDuplicateNumber.java
@@ -1,24 +1,20 @@
 public class _287FindTheDuplicateNumber {
     public int findDuplicate(int[] nums) {
-        int lo = 0, hi = nums.length - 1;
-        while(lo + 1 < hi){
+        int lo = 1, hi = nums.length -1;
+        while(lo <= hi){
             int mid = (hi - lo) / 2 + lo;
             int count = 0;
             for (int i = 0; i < nums.length; i += 1){
-                if (nums[i] - 1 <= mid){
+                if (nums[i] <= mid){
                     count += 1;
                 }
             }
-            if (count > mid + 1){
-                hi = mid;
+            if (count <= mid){
+                lo = mid + 1;
             }else{
-                lo = mid;
+                hi = mid - 1;
             }
         }
-        int count = 0;
-        for (int i = 0; i < nums.length; i += 1){
-            if (nums[i] - 1 == lo) count ++;
-        }
-        return (count > 1)? lo + 1: hi + 1;
+        return hi + 1;
     }
 }
